80ef23cbdb1a292ffa1c48caeef639d244fff8e6
sethi,akash <akash.sethi@aegon.nl>
2019-07-02 00:16:18 +0200

    removed mock data and integrated api.

--- src/api.js
+++ src/api.js
@@ -1,23 +1,26 @@
 import axios from 'axios';
-import {setJWT} from "./utils";
+import {setJWT} from "./utils/utils";
 
 const BASE_PATH = 'http://localhost:8080';
 
 const api = {
     createUser: data =>
-         axios.post(`${BASE_PATH}/user/save`, data).then(res => res.data),
+         axios.post(`${BASE_PATH}/user/save`, data).then(res => {
+             setJWT(res.headers['x-auth-token']);
+             return res.data
+         }),
     auth: data =>
         axios.post(`${BASE_PATH}/user/login`, data).then(res => {
             setJWT(res.headers['x-auth-token']);
             return res.data
         }),
     blogs : () =>
-        axios.get('./initialState.json').then(res => res.data.blogs),
+        axios.get(`${BASE_PATH}/blog`).then(res => res.data),
 
     blog : (id) =>
-        axios.get('./initialState.json').then(res => res.data),
+        axios.get(`${BASE_PATH}/blog/${id}`).then(res => res.data),
     addComment: data =>
-        axios.post(`${BASE_PATH}/comment/save`, data).then(res => res.data)
+        axios.post(`${BASE_PATH}/comment/${data.id}`, {comment: data.comment}, {}).then(res => res.data)
 };
 
 export default api;
